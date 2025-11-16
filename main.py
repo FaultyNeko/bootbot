5d3a194bff4b7ae623503f7df54f5042dd7ddc75
faultyneko <thefaultyjetpack@gmail.com>
2025-11-16 15:21:18 +0100

    main now can read a file as an input

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("greetings boots")
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+if __name__ == "__main__":
+    book_text = get_book_text("books/frankenstein.txt")
+    print(book_text)  # Print the first 1000 characters of the book text
